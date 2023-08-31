d4aa043474932d2e1cc79d812426b9a11311803f
Gladys254 <wangarimainagladys@gmail.com>
2023-08-31 05:03:53 -0700

    my assignment is here sir

---
+++ assignment.dart
@@ -0,0 +1,7 @@
+void main() {
+  var name = " Gladys Maina";
+  var school = " is in PLP ACADEMY";
+  var city = " in Nakuru";
+  var country = " ,Kenya";
+  print(name + school + city + country);
+}
